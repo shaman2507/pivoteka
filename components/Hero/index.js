bc90875bf7afe141b9623189481b2388edb59970
shaman2507 <alabay174@gmail.com>
2025-02-23 19:51:53 +0100

    hero-title
    Hero title mobile

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,11 +1,12 @@
 import initTranslations from '@/app/i18n';
+import { lora } from '@/fonts';
 
 const Hero = async ({ lng }) => {
     const { t } = await initTranslations(lng, ['hero']);
     return (
-        <section>
-            <div>
-                <h1>
+        <section className='mt-[80px]'>
+            <div className='flex justify-center'>
+                <h1 className={`${lora.className} text-center font-medium text-5xl text-orange-600`}>
                     {t('hero.title')}
                 </h1>
             </div>

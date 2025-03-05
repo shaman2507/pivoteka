5933f5dafb8d580534b9a0e0c0ffd2630469e9ee
shaman2507 <alabay174@gmail.com>
2025-03-05 20:26:35 +0100

    Hero desktop

--- components/Hero/quotes-list.jsx
+++ components/Hero/quotes-list.jsx
@@ -11,15 +11,15 @@ import ElementQuoteMob4 from '@/public/element-quote-4mob.svg';
 const HeroQuotesList = async ({ lng }) => {
     const { t } = await initTranslations(lng, ['hero']);
     return (
-        <conteiner className='w-full mt-[24px] md:mt-[116px] md:pl-[84px] md:pr-[52px] flex flex-col'>
-            <div className='flex justify-end md:justify-start md:gap-[88px]'>
-                <div className='hidden md:flex md:w-[246px]'>
+        <conteiner className='mx-auto w-full md:w-[512px] xl:w-[732px] mt-[24px] md:mt-[116px]  flex flex-col xl:justify-center'>
+            <div className='flex justify-end md:justify-start xl:justify-between md:gap-[88px]'>
+                <div className='hidden md:flex md:w-[246px] xl:[206px]'>
                     <ElementQuote1 className={'absolute z-50 md:mt-[-32px] md:w-[104px] md:h-[120px]'} />
                     <p className={`${inter.className} md:ml-[40px] font-medium italic text-base text-white-500`}>
                         {t('hero.quote-1')}
                     </p>
                 </div> 
-                <div className='flex w-[166px] md:w-[176px] mb-[95px] md:mb-[136px] items-center'>
+                <div className='flex w-[166px] md:w-[176px] xl:[166px] mb-[95px] md:mb-[136px] items-center'>
                     <p className={`${inter.className} font-medium italic text-base text-white-500`}>
                         {t('hero.quote-2')}
                     </p>
@@ -27,23 +27,25 @@ const HeroQuotesList = async ({ lng }) => {
                     <ElementQuote2 className={'hidden md:flex absolute z-50 md:ml-[96px] md:w-[104px] md:h-[120px]'} />
                 </div>    
             </div>
-            <div className='flex flex-col md:flex-row md:justify-end  mb-[64px]'>
-                <div className='flex w-[206px] md:w-[220px] items-center'>
-                    <p className={`${inter.className} font-medium italic text-base text-white-500`}>
-                        {t('hero.quote-3')}
-                    </p>
-                </div> 
-                <ElementQuoteMob3 className={'z-50 w-[180px] h-[36px] mt-[12px] md:hidden'} />
-                <ElementQuote3 className={'hidden md:flex absolute z-50 md:mt-[44px] md:w-[270px] md:h-[56px]'} />
-            </div>
-            <div className='flex justify-end md:justify-start md:mt-[16px]'>
-                <div className='flex w-[207px] md:w-[208px] items-center'>
-                    <p className={`${inter.className} font-medium italic text-base text-white-500`}>
-                        {t('hero.quote-4')}
-                    </p>
-                    <ElementQuoteMob4 className={'absolute z-50 mt-[64px] w-[180px] h-[36px] md:hidden'} />
-                    <ElementQuote4 className={'hidden md:flex absolute z-50 md:w-[280px] md:h-[56px] md:mt-[100px]'} />
-                </div>    
+            <div className='flex xl:justify-between'>
+                <div className='flex justify-end md:justify-start md:mt-[16px]'>
+                    <div className='flex w-[207px] md:w-[208px] xl:[204px] items-center'>
+                        <p className={`${inter.className} font-medium italic text-base text-white-500`}>
+                            {t('hero.quote-4')}
+                        </p>
+                        <ElementQuoteMob4 className={'absolute z-50 mt-[64px] w-[180px] h-[36px] md:hidden'} />
+                        <ElementQuote4 className={'hidden md:flex absolute z-50 md:w-[280px] md:h-[56px] md:mt-[100px]'} />
+                    </div>    
+                </div>
+                <div className='flex flex-col md:flex-row md:justify-end mb-[64px]'>
+                    <div className='flex w-[206px] md:w-[220px] xl:[204px] items-center'>
+                        <p className={`${inter.className} font-medium italic text-base text-white-500`}>
+                            {t('hero.quote-3')}
+                        </p>
+                    </div> 
+                    <ElementQuoteMob3 className={'z-50 w-[180px] h-[36px] mt-[12px] md:hidden'} />
+                    <ElementQuote3 className={'hidden md:flex absolute z-50 md:mt-[44px] md:w-[270px] md:h-[56px]'} />
+                </div>
             </div>
         </conteiner>
     );
--- components/Hero/quotes.jsx
+++ components/Hero/quotes.jsx
@@ -9,7 +9,7 @@ const HeroQuotes = async ({ lng }) => {
     const { t } = await initTranslations(lng, ['hero']);
     return (
         <section className='flex flex-col'>
-            <div className='mt-[24px] flex md:hidden justify-between'>
+            <div className='mt-[24px]  flex md:hidden justify-between'>
                 <div className='flex w-[232px] items-center md:hidden'>
                     <ElementQuoteMob1 className={'absolute z-50 w-[60px] h-[72px] mt-[24px]'} />
                     <p className={`${inter.className} ml-[24px] font-medium italic text-base text-white-500`}>
@@ -18,7 +18,7 @@ const HeroQuotes = async ({ lng }) => {
                 </div>    
                 <Social className={'md:hidden'}/>
             </div>
-            <div className='flex '>
+            <div className='flex justify-center md:justify-between'>
                 <HeroQuotesList lng={lng} />
                 <Social className={'hidden md:flex'} />
             </div>

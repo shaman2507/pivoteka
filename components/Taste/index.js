dd4142fb932786bea0236262bc0a56f6e6904ef3
shaman2507 <alabay174@gmail.com>
2025-03-18 19:12:20 +0100

    taste desktop

--- components/Taste/index.js
+++ components/Taste/index.js
@@ -14,19 +14,19 @@ import tasteListData from './tasteListData';
 const Taste = async ({ lng }) => { 
     const { t } = await initTranslations(lng, ['taste']);
     return (
-        <section className='mt-[100px] h-[660px] bg-black-500 md:px-[40px]'>
-            <div className='flex flex-col md:flex-row mb-[40px] md:mb-[44px] pt-[100px] md:pt-[120px] md:items-center justify-center md:justify-between'>
-                <h2 className={`${lora.className} w-[328px] md:w-[380px] font-medium text-[36px] md:text-[40px] xl:text-[64px] text-center md:text-left leading-[1.2] md:leading-[1.0] text-orange-600`}>
+        <section className='h-[660px] bg-black-500 md:px-[40px] xl:px-[60px] xlr:px-[80px]'>
+            <div className='flex flex-col md:flex-row mb-[40px] md:mb-[44px] xl:mb-[64px] pt-[100px] md:pt-[120px] md:items-center justify-center md:justify-between'>
+                <h2 className={`${lora.className} w-[328px] md:w-[380px] xl:w-[604px] font-medium text-[36px] md:text-[40px] xl:text-[64px] text-center md:text-left leading-[1.2] md:leading-[1.0] text-orange-600`}>
                     {t('taste.title')}
                 </h2>
                 <div className='hidden md:flex'>
                     <SwiperBtn />
                 </div>
             </div>
-            <div className='mx-auto w-[328px] md:w-[688px] h-[244px] md:mb-[120px]'>
+            <div className='mx-auto w-[328px] md:w-[688px] xl:w-[1160px] xlr:w-[1280px] h-[244px] md:mb-[120px]'>
                 <Swiper
                     wrapperClass="!items-stretch"
-                    className='mx-auto h-[244px]'
+                    className='h-[244px]'
                     autoHeight={true}
                     modules={[Navigation]}
                     loop={true}
--- components/Taste/tasteList.jsx
+++ components/Taste/tasteList.jsx
@@ -4,30 +4,25 @@ import { inter } from '@/fonts';
 const TasteList = async ({ lng, tasteTitle, tasteText, tasteTextSub }) => { 
     const { t } = await initTranslations(lng, ['taste']);
     return (
-        <div className='flex flex-col justify-center w-[328px] md:w-[335px] h-[244px] rounded-[8px] bg-white-600'>
-            
-            <div className='flex flex-col p-[32px]'>
-                <div className='flex '>
-                    <h4 className={`${inter.className}mx-auto mb-[16px] font-medium text-[20px] xl:text-[24px] leading-[1.2] text-orange-600`}>
-                        {t(tasteTitle)}
-                    </h4>
-                </div>
-                
-                <div className='mx-auto flex flex-col items-baseline gap-[16px]'>
-                    <div>
-                        <p className={`${inter.className} font-normal text-[16px] xl:text-[40px] leading-[1.3] text-white-500`}>
-                            {t(tasteText)}
-                        </p>
-                    </div>
-                    <div >
-                        <p className={`${inter.className} font-normal text-[16px] xl:text-[24px] leading-[1.3] text-white-500`}>
-                            {t(tasteTextSub)}
-                        </p>
-                    </div>
-                </div>
-                
+        <div className='flex flex-col  w-[328px] md:w-[336px] xl:w-[272px] xlr:w-[304px] h-[244px] xl:h-[248px] p-[32px] rounded-[8px] bg-white-600'>
+            <div className='flex '>
+                <h4 className={`${inter.className}mx-auto mb-[16px] font-medium text-[20px] xl:text-[24px] leading-[1.2] text-orange-600`}>
+                    {t(tasteTitle)}
+                </h4>
             </div>
-                    
+            
+            <div className='flex flex-col justify-between items-baseline gap-[16px]'>
+                <div>
+                    <p className={`${inter.className} font-normal text-[14px] leading-[1.3] text-white-500`}>
+                        {t(tasteText)}
+                    </p>
+                </div>
+                <div >
+                    <p className={`${inter.className} font-normal text-[14px] leading-[1.3] text-white-500`}>
+                        {t(tasteTextSub)}
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };

7393e11b69e3855511b08c66f0d9e1c6a2db66cf
shaman2507 <alabay174@gmail.com>
2025-03-30 19:26:24 +0200

    footer mob

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,37 +2,33 @@ import { inter } from '@/fonts';
 import initTranslations from '../../app/i18n';
 import FooterSocial from '../ui/footer-social';
 import Logo from '../ui/logo';
-import Link from 'next/link';
+import LocationFooter from '../ui/location-footer';
+import FooterNavigation from './footer-navigation';
 
 const Footer = async ({ lng }) => { 
     const { t } = await initTranslations(lng, ['footer']);
     return (
         <footer className='pt-[40px] h-[749px] bg-black-300'>
-            <div className='flex flex-col items-center'>
+            <div className='w-[164px] mx-auto flex flex-col items-center'>
                 <div className='flex flex-col'>
                     <div className="flex">
-                        <Logo className={'w-[80px] h-[80px]'} />
+                        <Logo className={'w-[120px] h-[120px]'} />
                     </div>
-                    <div className='flex flex-col mt-[64px] gap-[24px]'>
-                        <Link href="#about-us" className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
-                            {t('footer.about-us')}
-                        </Link>
-                        <Link href="#taste" className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
-                            {t('footer.beer')}
-                        </Link>
-                        <Link href="#best-offers" className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
-                            {t('footer.promotions')}
-                        </Link>
-                        <Link href="#contacts" className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
-                            {t('footer.contacts')}
-                        </Link>
+                    <div className='flex items-center'>
+                        <FooterNavigation />
                     </div>
                 </div>
-                <div className='mt-[64px]'>
-                    <h3 className={`${inter.className} font-normal text-[16px] leading-[1.3] text-white-500`}>
-                        {t('footer.find')}
-                    </h3> 
-                    <FooterSocial />
+                <div className='mt-[64px] mb-[80px]'>
+                    <div className='flex flex-col items-center'>
+                        <h3 className={`${inter.className}  font-normal text-[16px] leading-[1.3] text-white-500`}>
+                            {t('footer.find')}
+                        </h3> 
+                        <FooterSocial />
+                    </div>
+                    <LocationFooter />
+                </div>
+                <div>
+                    <span className={`${inter.className} font-normal text-[16px] leading-[1.3] text-white-500`}>© 2025 Pivoteka</span>
                 </div>
             </div>
         </footer>
--- components/ui/location.jsx
+++ components/ui/location.jsx
@@ -1,17 +1,18 @@
 import Link from 'next/link';
 import { IoLocationSharp } from "react-icons/io5";
 import { FaPhone } from "react-icons/fa6";
+import { inter } from '@/fonts';
 
 const LocationLink = ({ className, onClick }) => {
     return (
         <div className='flex items-center gap-[24px]'>
             <Link className={`${className}`} href="https://maps.app.goo.gl/3vWqMVqiBofgAFiu9" onClick={onClick}>
                 <IoLocationSharp color="orange" size="24" />
-                <p className='hidden xl:flex pl-[4px] text-white-500'>Budva</p>
+                <p className={`${inter.className} hidden xl:flex pl-[4px] text-white-500`}>Budva</p>
             </Link>
             <Link className={`${className}`} href="tel:+382222222222" onClick={onClick}>
                 <FaPhone color="orange" size="24" />
-                <p className='hidden xl:flex pl-[4px] text-white-500'>+382222222222</p>
+                <p className={`${inter.className} hidden xl:flex pl-[4px] text-white-500`}>+382222222222</p>
             </Link>
         </div>
     );

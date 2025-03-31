8870935094ef1ead5c454e9367c7be783da66898
shaman2507 <alabay174@gmail.com>
2025-03-31 22:58:08 +0200

    about us container

--- components/AboutUs/index.js
+++ components/AboutUs/index.js
@@ -10,16 +10,18 @@ const AboutUs = async ({ lng }) => {
     const { t } = await initTranslations(lng, ['aboutUs']);
     return (
         <section id="about-us" className='flex flex-col xl:flex-row bg-black-600'>
-            <div className='pt-[92px] md:pt-[120px] xlr:pt-[160px] px-[16px] md:px-[128px] xl:px-[80px] pb-[80px] xl:pb-[120px] xl:w-[50%]'>
-                <h2 className={`${lora.className} mb-[40px] xl:mb-[52px] text-medium text-[36px] md:text-[40px] xl:text-[64px] leading-[1.2] md:leading-[1.0] text-yellow-500`}>
-                    {t('aboutUs.title')}
-                </h2>
-                <p className={`${inter.className} mb-[20px] xl:mb-[24px] text-normal text-[16px] leading-[1.3] text-white-500`}>
-                    {t('aboutUs.text-1')}
-                </p>
-                <p className={`${inter.className}  text-normal text-[16px] leading-[1.3] text-white-500`}>
-                    {t('aboutUs.text-2')}
-                </p>
+            <div className='xl:flex xl:justify-end pt-[92px] md:pt-[120px] xlr:pt-[160px] px-[16px] md:px-[128px] xl:px-[80px] pb-[80px] xl:pb-[120px] xl:w-[50%]'>
+                <div className='xl:w-[412px] xl:h-[328px] xl:mr-[236px]'>
+                    <h2 className={`${lora.className} mb-[40px] xl:mb-[52px] text-medium text-[36px] md:text-[40px] xl:text-[64px] leading-[1.2] md:leading-[1.0] text-yellow-500`}>
+                        {t('aboutUs.title')}
+                    </h2>
+                    <p className={`${inter.className} mb-[20px] xl:mb-[24px] text-normal text-[16px] leading-[1.3] text-white-500`}>
+                        {t('aboutUs.text-1')}
+                    </p>
+                    <p className={`${inter.className}  text-normal text-[16px] leading-[1.3] text-white-500`}>
+                        {t('aboutUs.text-2')}
+                    </p>
+                </div>  
             </div>
             <div className='xl:w-[50%]'>
                 <Image src={HeroImg} alt="glass of beer" height={569} className='md:hidden' />

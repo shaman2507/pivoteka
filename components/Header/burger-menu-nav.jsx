1ce0e3f1a8d0c6bd6539128d1a8398eca9f97922
shaman2507 <alabay174@gmail.com>
2025-04-02 18:44:56 +0200

    burger menu fixed

--- components/Header/burger-menu-nav.jsx
+++ components/Header/burger-menu-nav.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import { inter } from '@/fonts';
 import initTranslations from '../../app/i18n';
 
-const BurgerMenuNav = ({ lng }) => {
+const BurgerMenuNav = ({ lng, closeBurgerMenu }) => {
   const [t, setT] = useState(() => (key) => key);
 
   useEffect(() => {
@@ -18,16 +18,16 @@ const BurgerMenuNav = ({ lng }) => {
 
   return (
     <nav className="flex flex-col gap-[12px]">
-      <Link href="#about-us" className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
+      <Link href="#about-us" onClick={closeBurgerMenu} className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
         {t('header.about-us')}
       </Link>
-      <Link href="#taste" className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
+      <Link href="#taste" onClick={closeBurgerMenu} className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
         {t('header.beer')}
       </Link>
-      <Link href="#best-offers" className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
+      <Link href="#best-offers" onClick={closeBurgerMenu} className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
         {t('header.promotions')}
       </Link>
-      <Link href="#contacts" className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
+      <Link href="#contacts" onClick={closeBurgerMenu} className={`${inter.className} font-medium text-[16px] leading-[1.2] text-white-500`}>
         {t('header.contacts')}
       </Link>
     </nav>

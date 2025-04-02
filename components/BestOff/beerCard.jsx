214a24d4a9a56834cd5bcc3eb8cf879d9872c232
shaman2507 <121162304+shaman2507@users.noreply.github.com>
2025-04-02 22:29:32 +0200

    Revert "Best Off section fixed"

--- components/BestOff/beerCard.jsx
+++ components/BestOff/beerCard.jsx
@@ -5,18 +5,18 @@ import { inter } from '@/fonts';
 const BeerCard = async ({ lng, image, imageAlt, cardTitle, cardPrice, cardSize }) => {
     const { t } = await initTranslations(lng, ['bestOff']);
     return (
-        <div className='flex flex-col w-[328px] md:w-[335px] xl:w-[1160px] xlr:w-[1280px] h-[528px] md:h-[544px] xl:h-[546px] xlr:h-[624px] gap-[24px]'>
-            <div className='w-[328px] md:w-[335px] xl:w-[300px] h-[420px] md:h-[432px] xl:h-[400px] flex justify-center'>
+        <div className='flex flex-col justify-center w-[328px]'>
+            <div className='w-[328px] xl:w-[300px] h-[420px] xl:h-[400px] flex justify-center'>
                 <Image src={image} alt={t(imageAlt)} width={300} height={400}  />
             </div>
-            <div className='flex flex-col justify-between text-center w-[328px] md:w-[335px] xl:w-[300px] xl:h-[116px]'>
+            <div className='flex flex-col'>
                 <div className='flex justify-center'>
-                    <h4 className={`${inter.className} mx-auto font-medium text-[20px] xl:text-[24px] leading-[1.2] text-black-600`}>
+                    <h4 className={`${inter.className}mx-auto mt-[24px] mb-[36px] font-medium text-[20px] xl:text-[24px] leading-[1.2] text-black-600`}>
                         {t(cardTitle)}
                     </h4>
                 </div>
                 
-                <div className='mx-auto flex items-baseline'>
+                <div className='mx-auto mb-[40px] flex items-baseline'>
                     <div>
                         <p className={`${inter.className} font-medium text-[28px] md:text-[32px] xl:text-[40px] text-black-600`}>
                             {t(cardPrice)}
@@ -26,9 +26,12 @@ const BeerCard = async ({ lng, image, imageAlt, cardTitle, cardPrice, cardSize }
                         <p className={`${inter.className} font-medium text-[18px] md:text-[20px] xl:text-[24px] leading-[1.2] text-black-600`}>
                             {t(cardSize)}
                         </p>
-                    </div>   
-                </div>  
+                    </div>
+                    
+                </div>
+                
             </div>
+            
         </div>
     );
 };
--- components/BestOff/index.js
+++ components/BestOff/index.js
@@ -14,7 +14,7 @@ import { SwiperBtn } from '../Buttons/SwiperBtn';
 const BestOff = async ({ lng }) => {
     const { t } = await initTranslations(lng, ['bestOff']);
     return (
-        <section id="best-offers" className='h-[900px] md:h-[872px] xl:h-[884px]'>
+        <section id="best-offers" className='mb-[100px]'>
             <div className='flex justify-center md:justify-between md:items-center mt-[100px] md:mt-[124px] mb-[40px] md:mb-[44px]'>
                 <h2 className={`${lora.className} font-medium text-[36px] md:text-[40px] xl:text-[64px] leading-[1.2] md:leading-[1.0] text-black-600`}>
                     {t('bestOff.title')}
@@ -24,7 +24,7 @@ const BestOff = async ({ lng }) => {
                 </div>
             </div>
             
-            <div className='mx-auto md:w-[688px] xl:w-[1160px] xlr:w-[1280px] h-[616px] md:h-[544px] xl:h-[546px]'>
+            <div className='mx-auto w-[328px] md:w-full h-[616px] md:h-full'>
                 <Swiper
                     wrapperClass="!items-stretch"
                     className='mx-auto'

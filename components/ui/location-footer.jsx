86e98e9541d7fbc7d8b140fb687a2861e42261ab
shaman2507 <alabay174@gmail.com>
2025-04-04 19:21:03 +0200

    change lang fixed

--- components/ui/location-footer.jsx
+++ components/ui/location-footer.jsx
@@ -8,11 +8,11 @@ const LocationFooter = ({ className, onClick }) => {
         <div className='mt-[52px] flex flex-col gap-[12px]'>
             <Link className={`${className} flex`} href="https://maps.app.goo.gl/3vWqMVqiBofgAFiu9" onClick={onClick}>
                 <IoLocationSharp className="text-yellow-500 hover:text-yellow-600 active:text-yellow-700 disabled:text-yellow-700 disabled:opacity-30 disabled:cursor-not-allowed transition-colors duration-200" size="24" />
-                <p className={`${inter.className} pl-[4px] my-auto font-medium text-[16px] leading-[1.2] text-white-500`}>Budva</p>
+                <p className={`${inter.className} pl-[4px] my-auto font-medium text-[16px] leading-[1.2] text-white-500 hover:text-yellow-600`}>Budva</p>
             </Link>
             <Link className={`${className} flex`} href="tel:+382222222222" onClick={onClick}>
                 <FaPhone className="text-yellow-500 hover:text-yellow-600 active:text-yellow-700 disabled:text-yellow-700 disabled:opacity-30 disabled:cursor-not-allowed transition-colors duration-200" size="24" />
-                <p className={`${inter.className} pl-[4px] my-auto font-medium text-[16px] leading-[1.2] text-white-500`}>+382222222222</p>
+                <p className={`${inter.className} pl-[4px] my-auto font-medium text-[16px] leading-[1.2] text-white-500 hover:text-yellow-600`}>+382222222222</p>
             </Link>
         </div>
     );
--- components/ui/location.jsx
+++ components/ui/location.jsx
@@ -8,11 +8,11 @@ const LocationLink = ({ className, onClick }) => {
         <div className='flex items-center gap-[24px]'>
             <Link className={`${className}`} href="https://maps.app.goo.gl/3vWqMVqiBofgAFiu9" onClick={onClick}>
                 <IoLocationSharp className="text-yellow-500 hover:text-yellow-600 active:text-yellow-700 disabled:text-yellow-700 disabled:opacity-30 disabled:cursor-not-allowed transition-colors duration-200" size="24" />
-                <p className={`${inter.className} hidden xl:flex pl-[4px] text-white-500 `}>Budva</p>
+                <p className={`${inter.className} hidden xl:flex pl-[4px] text-white-500 hover:text-yellow-600`}>Budva</p>
             </Link>
             <Link className={`${className}`} href="tel:+382222222222" onClick={onClick}>
                 <FaPhone className="text-yellow-500 hover:text-yellow-600 active:text-yellow-700 disabled:text-yellow-700 disabled:opacity-30 disabled:cursor-not-allowed transition-colors duration-200" size="24" />
-                <p className={`${inter.className} hidden xl:flex pl-[4px] text-white-500 `}>+382222222222</p>
+                <p className={`${inter.className} hidden xl:flex pl-[4px] text-white-500 hover:text-yellow-600`}>+382222222222</p>
             </Link>
         </div>
     );
